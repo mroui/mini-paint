38428cdad7b65e19d44e76f5b0eb62d74c616532
Martyna Roj <martynaxroj@gmail.com>
2019-09-14 20:17:26 +0200

    Refactor describing comments & translate to english

--- Headers/MainWindow.h
+++ Headers/MainWindow.h
@@ -23,9 +23,9 @@ public:
     explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow() override;
 
-    void setColorBiggerFunction();              //sprawdza ktory kolor (button) zostal wciśnięty i ten kolor ustawia na pędzlu w canvasie
-    void ifFillThenColor(QColor);               //sprawdza czy nacisnieto przycisk wypelnienia wtedy ustawia kolor pedzla/wypelnienia
-    void setShapes(QString);                    //ustawia kolor pedzla + ustawia ksztalty na false poza tym w argumencie
+    void setColorBiggerFunction();              //set brush color by check selected color on palette
+    void ifFillThenColor(QColor);               //set fill color or brush color
+    void setShapes(QString);                    //set shape of pencil
 
 private slots:
     void on_Bialy_clicked();
@@ -33,7 +33,7 @@ private slots:
     void on_Szary_clicked();
     void on_Czerwony_clicked();
     void on_Zolty_clicked();
-    void on_Niebieski_clicked();                //eventy buttonclicked kolorow
+    void on_Niebieski_clicked();
     void on_Zielony_clicked();
     void on_Rozowy_clicked();
     void on_Fioletowy_clicked();
@@ -41,42 +41,40 @@ private slots:
     void on_palette_2_clicked();
 
 protected:
-    void closeEvent(QCloseEvent *) override;    //event zamykania okna
+    void closeEvent(QCloseEvent *) override;
 
 private:
     Ui::MainWindow *ui;
-
-    Canvas * canvas;                            //obiekt obrazu
-    QColor previouspalettecolor;                //poprzedni kolor wybrany z palety (dialogcolor) - by go zapamiętać
-    QSizeGrip * sizeGrip;                       //suwak do zwiększania obrazu
+    Canvas * canvas;                            //drawing object
+    QColor previouspalettecolor;                //previous checked color in palette
+    QSizeGrip * sizeGrip;                       //grip to increase image
 
 private slots:
     void on_ButtonGumka_clicked();
     void on_ButtonOlowek_clicked();
     void on_SpinBoxGruboscLinii_valueChanged(int);
     void on_ButtonLinia_clicked();
-    void on_ButtonKwadrat_clicked();            //eventy buttonclicked/valuechanged na narzedziach (kolo, trojkat, olowek, gumka, wypelnienie...)
+    void on_ButtonKwadrat_clicked();            //clicked/valuechanged events on toolbar
     void on_ButtonKolo_clicked();
     void on_ButtonTrojkat_clicked();
     void on_ButtonWypelnienie_clicked();
 
-
     void on_action_Nowy_triggered();
     void on_action_Otworz_triggered();
-    void on_actionZapisz_jako_triggered();      //eventy dla menubar/Plik: nowy, otworz, zapisz, zakoncz
+    void on_actionZapisz_jako_triggered();      //menubar/file events
     void on_actionZako_cz_triggered();
 
     void on_actionWyczy_obraz_triggered();
     void on_actionOdwr_kolory_triggered();
-    void on_actionOdwr_triggered();             //eventy dla menubar/Obraz: wyczyscObraz, odwrocKolory, odwrocObraz, zmienRozmiar, rozciagnij, wytnij, wklej
+    void on_actionOdwr_triggered();             //menubar/image events
     void on_actionZmie_rozmiar_triggered();
     void on_actionRozci_gnij_triggered();
     void on_actionWytnij_triggered();
     void on_actionWklej_triggered();
 
-    void on_actionZmie_kolor_triggered();       //event dla menubar/Kolory: zmien kolor
+    void on_actionZmie_kolor_triggered();       //menubar/colors events
 
-    void on_actionO_Oprogramie_triggered();     //event dla menubar/Pomoc: o programie
+    void on_actionO_Oprogramie_triggered();     //menubar/help events
 };
 
 #endif // MAINWINDOW_H
