343e2565ee0e083bfade96b2ae5b2c719428c1c9
Martyna Roj <martynaxroj@gmail.com>
2019-09-16 00:13:25 +0200

    Translate & add describing comments & optimize StrechImage source file

--- Sources/StretchImage.cpp
+++ Sources/StretchImage.cpp
@@ -1,14 +1,14 @@
 #include "Headers/StretchImage.h"
 #include "ui_StretchImage.h"
 
+
 stretchimg::stretchimg(QWidget *parent) : QDialog(parent), ui(new Ui::stretchimg)
 {
     ui->setupUi(this);
     setWindowFlags(Qt::WindowTitleHint | Qt::WindowCloseButtonHint | Qt::Dialog | Qt::MSWindowsFixedSizeDialogHint);
     set_textedit_height(100);
-    set_textedit_width(100);        //defaultowo ustawia 100%
+    set_textedit_width(100);        //100% by default
 }
-
 
 
 stretchimg::~stretchimg()
@@ -17,13 +17,15 @@ stretchimg::~stretchimg()
 }
 
 
+//set/get methods
+//---------------------------------------------------------------------------------
+
 
 void stretchimg::set_textedit_height(int nmb)
 {
     QString nmb_str = QString::number(nmb);
     ui->te_wysokosc->setText(nmb_str);
 }
-
 
 
 int stretchimg::get_textedit_height()
@@ -34,13 +36,11 @@ int stretchimg::get_textedit_height()
 }
 
 
-
 void stretchimg::set_textedit_width(int nmb)
 {
     QString nmb_str = QString::number(nmb);
     ui->te_szerokosc->setText(nmb_str);
 }
-
 
 
 int stretchimg::get_textedit_width()
